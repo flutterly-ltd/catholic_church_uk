6f5a8426a803fa88ea57d7ac08c3081b1d9dcfb4
Anoop Jose <anoop@flutterly.co.uk>
2021-09-13 22:40:34 +0100

    Initial Commit

---
+++ catholic_church_uk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  catholic_church_uk
+//
+//  Created by Anoop Jose on 13/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ catholic_church_uk/catholic_church_ukApp.swift
@@ -0,0 +1,17 @@
+//
+//  catholic_church_ukApp.swift
+//  catholic_church_uk
+//
+//  Created by Anoop Jose on 13/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct catholic_church_ukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
